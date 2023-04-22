32a45c79003039d890e6adc717a9ccc09d16cb7c
Bajrang Kadam <bajrangkadam9@gmail.com>
2023-04-22 16:32:43 +0530

    Enum changed

--- utils/enums.js
+++ utils/enums.js
@@ -1,9 +1,9 @@
 module.exports.notification = {
     0: 'reject view profile request',
-    1: 'send view profile request',
+    1: 'sent view profile request',
     2: 'accept view profile request',
     3: 'seen your profile',
     4: 'liked your profile',
     5: 'saved your profile',
-    6: 'send message'
+    6: 'sent message'
   }
